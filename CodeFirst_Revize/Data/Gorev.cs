df8efc85be86c3fd21afd87301f6bd87a3778284
EdaOzge <eozge.ugurlu@tedu.edu.tr>
2025-01-05 23:58:55 +0300

    Delete CodeFirst_Revize directory

--- CodeFirst_Revize/Data/Gorev.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CodeFirst_Revize.Data
-{
-    public class Gorev
-    {
-        public int Id { get; set; }
-        public string Baslik { get; set; } = "";
-        public bool Yapildi { get; set; }
-    }
-}
